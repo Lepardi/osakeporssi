ae2d897f61105e3cb1d815bb8e6d9fef3536938b
lepardi <lepardi.km@gmail.com>
2025-07-04 11:09:08 +0300

    When listing a new company add it into listers portfolio

--- exchange.py
+++ exchange.py
@@ -9,3 +9,16 @@ def new_listing(company_name, stock_amount, lister_name):
     sql = """INSERT INTO companies (name, stock_amount, last_price, owner) 
             VALUES (?, ?, ?, ?)"""
     db.execute(sql, [company_name, stock_amount, 0, lister_name])
+    add_to_portfolio(lister_name, company_name, stock_amount)
+
+def add_to_portfolio(username, company_name, stock_amount):
+    sql = "SELECT id FROM companies WHERE name = ?" 
+    company_id = db.query(sql, [company_name])
+
+    sql = "SELECT id FROM users WHERE username = ?" 
+    user_id = db.query(sql, [username])
+
+    sql = """INSERT INTO portfolios (user_id, company_id, amount) 
+            VALUES (?, ?, ?)"""
+    db.execute(sql, [user_id[0]["id"], company_id[0]["id"], stock_amount])
+
